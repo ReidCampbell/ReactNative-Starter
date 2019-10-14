f9f78254955e2980f4e26c77954255e627b8829e
ReidCampbell <reidscottcampbell@gmail.com>
2019-10-14 22:42:04 +0100

    made a resuable component

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,8 +1,24 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
-const HomeScreen = () => {
-    return <Text style={styles.text}>Hello</Text>;
+const HomeScreen = ({ navigation }) => {
+    return (
+        <View>
+            <Text style={styles.text}>Hello</Text>
+            <Button
+                title="Go to Components Demo"
+                onPress={() => navigation.navigate("Components")}
+            />
+            <Button
+                onPress={() => navigation.navigate("List")}
+                title="Go to List Demo"
+            />
+            <Button
+                onPress={() => navigation.navigate("Image")}
+                title="Go to Image Demo"
+            />
+        </View>
+    );
 };
 
 const styles = StyleSheet.create({
